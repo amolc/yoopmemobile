a09737ab9b4bd2f774d67e1c3865e56ed1d21d8a
amolv <amolvhankalas@gmail.com>
2014-09-27 16:31:17 +0530

    new files

---
+++ www/js/controllers.js
@@ -0,0 +1,17 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {
+})
+
+.controller('FriendsCtrl', function($scope, Friends) {
+  $scope.friends = Friends.all();
+})
+
+.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
+  $scope.profile = Friends.get($stateParams.friendId);
+})
+
+.controller('CreateCtrl', function($scope) {
+})
+.controller('AccountCtrl', function($scope) {
+});
---
+++ www/js/services.js
@@ -0,0 +1,29 @@
+angular.module('starter.services', [])
+
+/**
+ * A simple example service that returns some data.
+ */
+.factory('Friends', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var friends = [
+    { id: 0, name: 'Scruff McGruff' },
+    { id: 1, name: 'G.I. Joe' },
+    { id: 2, name: 'Kate Winslet',
+     description : 'Beautiful and witty. I love people who can make me smile.',
+      likes : 32,
+      url : './profile-pics/kate.jpg' },
+    { id: 3, name: 'Ash Ketchum' }
+  ];
+
+  return {
+    all: function() {
+      return friends;
+    },
+    get: function(friendId) {
+      // Simple index lookup
+      return friends[friendId];
+    }
+  }
+});
