0b4c59905fb6a4e861f4fabcf02b280464e541fe
chwasimarshad <wasim@fortsolution.com>
2014-10-01 19:06:30 +0500

    settign adn event prefernce

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','openfb'])
+angular.module('starter', ['ionic', 'starter.controllers','uiSlider', 'starter.services','openfb'])
 
 .run(function($rootScope, $state, $ionicPlatform, $window, OpenFB) {
   $ionicPlatform.ready(function() {
