988db1165aa1e721533e30b696dff988323ef837
Amol <amol.chawathe@fountaintechies.com>
2014-10-04 21:12:15 +0800

    app.js FB app ID and cordova.js added

--- www/js/app.js
+++ www/js/app.js
@@ -20,8 +20,8 @@ angular.module('starter', ['ionic', 'starter.controllers','uiSlider', 'starter.s
     }
   });
 
-  OpenFB.init('1436843073264106','https://www.facebook.com/connect/login_success.html',$window.localStorage);
 
+ OpenFB.init('1436843073264106','https://www.facebook.com/connect/login_success.html',$window.localStorage);
         $ionicPlatform.ready(function () {
             if (window.StatusBar) {
                 StatusBar.styleDefault();
