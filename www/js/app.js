673718d70fa2616313da9004c1534668a6cb7c8e
chwasimarshad <chouhdarywasim@gmail.com>
2014-10-19 16:59:08 +0500

    debug 3

--- www/js/app.js
+++ www/js/app.js
@@ -37,51 +37,51 @@ angular.module('starter', ['ionic', 'starter.controllers','uiSlider', 'starter.s
         //  });
    $rootScope.token = '1';
 
-    var androidConfig = {
-    "senderID":"240462080413",
-    "ecb":"onNotification"
-  };
+  //   var androidConfig = {
+  //   "senderID":"240462080413",
+  //   "ecb":"onNotification"
+  // };
 
-    var iosConfig = {
-        "badge":"true",
-        "sound":"true",
-        "alert":"true",
-        "ecb":"onNotificationAPN"
-      };
-
-
-
-      $cordovaPush.register(iosConfig).then(function(result) {
-          // Success! 
-          alert( result ) ;
-          console.log( result );
-          $rootScope.token = result;
-      }, function(err) {
-         alert(err);
-      });
-
-      $cordovaPush.register(androidConfig).then(function(result) {
-          // Success! 
-          alert( result ) ;
-          console.log( result );
-          $rootScope.token = result;
-      }, function(err) {
-         alert(err);
-      });
+  //   var iosConfig = {
+  //       "badge":"true",
+  //       "sound":"true",
+  //       "alert":"true",
+  //       "ecb":"onNotificationAPN"
+  //     };
 
 
-      $cordovaPush.unregister(options).then(function(result) {
-          // Success! 
-      }, function(err) {
-          // An error occured. Show a message to the user
-      });
 
-      // iOS only
-      $cordovaPush.setBadgeNumber(2).then(function(result) {
-          // Success! 
-      }, function(err) {
-          alert(err);
-      });  
+  //     $cordovaPush.register(iosConfig).then(function(result) {
+  //         // Success! 
+  //         alert( result ) ;
+  //         console.log( result );
+  //         $rootScope.token = result;
+  //     }, function(err) {
+  //        alert(err);
+  //     });
+
+  //     $cordovaPush.register(androidConfig).then(function(result) {
+  //         // Success! 
+  //         alert( result ) ;
+  //         console.log( result );
+  //         $rootScope.token = result;
+  //     }, function(err) {
+  //        alert(err);
+  //     });
+
+
+  //     $cordovaPush.unregister(options).then(function(result) {
+  //         // Success! 
+  //     }, function(err) {
+  //         // An error occured. Show a message to the user
+  //     });
+
+  //     // iOS only
+  //     $cordovaPush.setBadgeNumber(2).then(function(result) {
+  //         // Success! 
+  //     }, function(err) {
+  //         alert(err);
+  //     });  
 
 })
 
